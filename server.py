185a661d02c32f2de335a45efcd9302b267545d0
alexcercos <44820718+alexcercos@users.noreply.github.com>
2025-06-21 12:38:40 +0200

    Add flask CORS
    Se necesita en flutter para que acepte los endpoints, parece ser (en navegador al menos)

--- server.py
+++ server.py
@@ -1,8 +1,10 @@
 from flask import Flask, jsonify, request
 import database as db
 
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app)  # Allow all origins (for development)
 
 current_exercises = {}
 
